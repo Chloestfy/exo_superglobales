27b3a999f75884383d1eb25779f8d336f50b787e
Chloé Dijoux <chloedijoux@Mac-001.lan>
2025-09-25 12:36:01 +0200

    changement

--- exo1/exo1.php
+++ exo1/exo1.php
@@ -1,10 +1,19 @@
 <?php
 
+echo "<h3>Paramètres de l'URL</h3>";
 
-echo "<h3>Contenu de \$_GET</h3>";
-print_r($_GET);
+if (isset($_GET['ville'])) {
+    echo "Ville : " . htmlspecialchars($_GET['ville']) . "<br>";
+} else {
+    echo "Ville : paramètre non défini<br>";
+}
+
+if (isset($_GET['pays'])) {
+    echo "Pays : " . htmlspecialchars($_GET['pays']) . "<br>";
+} else {
+    echo "Pays : paramètre non défini<br>";
+}
 
 echo "<h3>Infos serveur</h3>";
-echo "Méthode : " . $_SERVER['REQUEST_METHOD'] . "<br>";
-echo "Script : " . $_SERVER['PHP_SELF'] . "<br>";
-echo "IP : " . $_SERVER['REMOTE_ADDR'] . "<br>";
+echo "Méthode HTTP : " . $_SERVER['REQUEST_METHOD'] . "<br>";
+echo "Nom du script : " . basename($_SERVER['PHP_SELF']) . "<br>";
